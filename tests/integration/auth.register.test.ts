8e9988781f62ee2852547dc6548e2ebf836674a3
habiburRhaman05 <devhabib2005@gmail.com>
2026-05-12 23:21:48 +0600

    no update

--- tests/integration/auth.register.test.ts
+++ tests/integration/auth.register.test.ts
@@ -8,62 +8,56 @@ import { AuthProvider, UserStatus } from "../../src/generated/prisma/enums";
 
 describe("POST /api/v1/auth/register", () => {
     beforeEach(async () => {
-
         await prisma.session.deleteMany();
         await prisma.user.deleteMany();
     });
 
-    // it("should register user and send OTP email", async () => {
-    //     const res = await request(app)
-    //         .post("/api/auth/register")
-    //         .send({
-    //             name: "Habibur Rahman",
-    //             email: "habibur@test.com",
-    //             password: "StrongPass@123",
-    //             authProvider: AuthProvider.local,
-    //             status: UserStatus.active,
-    //             role: "customer",
-    //             agreeTerms:true
-    //         });
+    it("should register user and send OTP email", async () => {
+        const res = await request(app)
+            .post("/api/auth/register")
+            .send({
+                name: "Habibur Rahman",
+                email: "habibur@test.com",
+                password: "StrongPass@123",
+                authProvider: AuthProvider.local,
+                status: UserStatus.active,
+                role: "customer",
+                agreeTerms:true
+            });
 
+        expect(res.statusCode).toBe(201);
+        const user = await prisma.user.findUnique({
+            where: { email: "habibur@test.com" }
+        });
+        expect(user).toBeTruthy();
+        expect(user?.emailVerified).toBe(false); 
+    });
 
-    //     expect(res.statusCode).toBe(201);
-    //     // expect(res.body).toHaveProperty("token"); // verify-email redirect token
-    //     // DB তে user created হয়েছে কিনা
-    //     const user = await prisma.user.findUnique({
-    //         where: { email: "habibur@test.com" }
-    //     });
-    //     expect(user).toBeTruthy();
-    //     expect(user?.emailVerified).toBe(false); // এখনো verify হয়নি
-    // });
+    it("should reject duplicate email", async () => {
+        await request(app).post("/api/auth/register")
+            .send({ 
 
-    // it("should reject duplicate email", async () => {
-    //     await request(app).post("/api/auth/register")
-    //         .send({ 
+                 name: "Habibur Rahman",
+                email: "habibur@test.com",
+                password: "StrongPass@123",
+                authProvider: AuthProvider.local,
+                status: UserStatus.active,
+                role: "customer",
+                agreeTerms:true
+            });
 
-    //              name: "Habibur Rahman",
-    //             email: "habibur@test.com",
-    //             password: "StrongPass@123",
-    //             authProvider: AuthProvider.local,
-    //             status: UserStatus.active,
-    //             role: "customer",
-    //             agreeTerms:true
-    //         });
-
-    //     const res = await request(app).post("/api/auth/register")
-    //         .send({
-    //              name: "Habibur Rahman",
-    //             email: "habibur@test.com",
-    //             password: "StrongPass@123",
-    //             authProvider: AuthProvider.local,
-    //             status: UserStatus.active,
-    //             role: "customer",
-    //             agreeTerms:true
-    //         });
+        const res = await request(app).post("/api/auth/register")
+            .send({
+                 name: "Habibur Rahman",
+                email: "habibur@test.com",
+                password: "StrongPass@123",
+                authProvider: AuthProvider.local,
+                status: UserStatus.active,
+                role: "customer",
+                agreeTerms:true
+            });
 
        
-            
-
-    //     expect(res.status).toBe(400);
-    // });
+        expect(res.status).toBe(400);
+    });
 });
